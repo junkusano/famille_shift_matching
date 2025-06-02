20e9e4d660954b26a56943452f01b48e98522752
junkusano <junkusano@shi-on.net>
2025-06-02 13:35:34 +0900

    fix: convert next.config.ts to js with eslint disabled

--- next.config.js
+++ next.config.js
@@ -1,7 +1,9 @@
 // next.config.js
 /** @type {import('next').NextConfig} */
-module.exports = {
+const nextConfig = {
   eslint: {
     ignoreDuringBuilds: true,
   },
 };
+
+module.exports = nextConfig;
