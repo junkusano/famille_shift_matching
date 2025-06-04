6369cffd0f3ea377502b34f0ffe93fd9043c1817
junkusano <junkusano@shi-on.net>
2025-06-04 21:24:38 +0900

    fix error of signup take4

--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -1,7 +1,7 @@
 'use client'
 
 import { useState } from 'react'
-import { useRouter } from 'next/navigation'
+//import { useRouter } from 'next/navigation'
 import { supabase } from '@/lib/supabaseClient'
 
 export default function SignupPage() {
