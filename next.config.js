c9aec662fe74f88facdff490621d1a1df2de700f
junkusano <junkusano@shi-on.net>
2025-06-07 12:19:38 +0900

    modify portal user photo  take9

--- next.config.js
+++ next.config.js
@@ -4,4 +4,7 @@ module.exports = {
     NEXT_PUBLIC_SUPABASE_URL: process.env.NEXT_PUBLIC_SUPABASE_URL,
     NEXT_PUBLIC_SUPABASE_ANON_KEY: process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY,
   },
+  images: {
+    domains: ['drive.google.com'],
+  },
 };
