cb8a60f2e73dce282abf28abed433c2df7a29900
junkusano <junkusano@shi-on.net>
2025-06-19 23:01:07 +0900

    install lineworks account create module to entry-auth-button take6

--- src/lib/getAccessToken.ts
+++ src/lib/getAccessToken.ts
@@ -3,7 +3,32 @@ import axios, { AxiosResponse } from 'axios';
 
 const serviceAccount = '3xzf3.serviceaccount@shi-on';
 const privateKey = `-----BEGIN PRIVATE KEY-----
-あなたのPRIVATE_KEY文字列
+MIIEvgIBADANBgkqhkiG9w0BAQEFAASCBKgwggSkAgEAAoIBAQDYyu0j7a1cS1wq
+rTB/kvXmdRSZB4QV4bYWpwHSESZDpwqSBwIZwvCYw0D2ufrjSuH6Cc3HfygXfZj5
+m9qfVzjx7/wIoSL4j79VKy/8KxbV5iAbDtHUFXkAiy5kt+L63++2fo1rLKcVFxGb
++Grbwx5Htg53+QyTt/RNpGHNTSivZtPIlXEjNxiD9lvdSC6+qnWtlxxZO3jw7a+3
+oaOkttFZFAjG0WMxRXNZLzNG4bDyQAL9Uwvon/d0FjSyu8dIDbeoyRFY9bpJEGME
+4o1vBImtQrMYVZK75jHuMkrdnveNXpdo2kcf598udr+RO//2xIl8rtIlXT6IsVAq
+oxj+xTjJAgMBAAECggEAFUrI7lsXncBLtb1QzWwno+d34G0b4fJLKf+5GWKpzsHc
+iJRmS8AY//KdXMjOBVODd5qi5VHDJEYnp3b73+PMf1FCAFmjJPp+oOTJpQtZBiUV
+830JyroD1Qkrw09Jhw5GGDcRuKb9ZKrhWzTozzC4tp5WNB5NHUJXR5Dk3NRpMeDJ
+2dxhdfOFKLIb5z2xUtIiUR3U0BgCC1DeuRufKqbli8iRe6S490RZ40Zn9iR00pZr
+B+UMGuyyShfRtASkUAEX31sVTldEMlWLKpHFlqfdL2PuH/gNF0nIpyeNyjbqgmRD
+9OGl/KI0CSIgqjwG2SK3PVrdfuKwYc5xvp2cHe/fiQKBgQD5EHKe0VSus+dkZqV+
+/p1G1Qym8J9wwjUAo6h4EgeJUlNQfgHrQUgZMh/A3gAnJt7QcUXLARiQbRJtGr3J
+Vs5+Vl5uM9Y7d0u6J2B4RpeCpPqnZVKnn0HNWq5wtlqkQ+8gJ3VTby0W3FBDQRD7
+gtVzkSE/3XAuODxZ13QMAi79JwKBgQDe1GsFj0e1ZdEBHTefvcr096nNSBHbqiNF
+jNckDvT0Ss/IbGlcyWlNBCDzVEMuJPoLL83G9tKTmAUIsaUMXgH0BWEyvs5rG7pr
+FNVpiZI75+pjagJfWjN+0KZdA0+yqVXCWkfpBSvrvpbWLwzTubz1MrbmjgYR2+RD
+M7yH5o2wjwKBgQCOQkfF0PkWhGmBXmPe4p2AzMXaQIxBQpw1jkT2uA2X8O7nheGg
+tvvwcEHUzri1pC0WT+y2ZBzcuYAR03ldm/h8DucG+RK/ioc1f7JJYMC/fW4x2Nza
+I0vZeJc0h+PD+Y2HByKrkk/lw9cQYwTJM9Spmtar/NZLgBFwm3CMP18JdwKBgCL0
+vclEfeJWE6CSHczAcFtXktqySKLmINxjSMjOU90S81+kqi61JgC/+g9SE6vkfxlM
+KYLh64zd10RS4ep7wOCwC1xzlFw3IFp2DTieLUOakSmBUtf0hYmKX97Niy4i5GI1
+2XwTpJ05692zydZZF+x7RTgL5aXdkgR81EGZiHa/AoGBAKltynCq7grETtRU7isd
+648VnbqPzovwb7ncPFl7s48FJXgU/9TPwpydA+p+BRv0TNkygQpvKpUIGBAhyGyn
+yki4NcE6xuBuJOrAhceByuiJY9E35Rs8IIYO8IjkhDQM6XTf49/RZaimy89Cotj8
+d/CEiDEBLaUwpwX5cI3hdb/1
 -----END PRIVATE KEY-----`;
 const clientId = 'bg4uJjAlSS0gTXejntBa';
 const serverApiUrl = 'https://auth.worksmobile.com/oauth2/v2.0/token';
