bb29f3f91252204606655860e0cb9d9849b21e96
junkusano <junkusano@shi-on.net>
2025-06-24 17:17:46 +0900

    install rpa_tmp list take18

--- src/app/portal/layout.tsx
+++ src/app/portal/layout.tsx
@@ -139,7 +139,7 @@ export default function PortalLayout({ children }: Props) {
                     <hr className="border-white my-2" />
                 </div>
             </div>
-            <div className="flex-1 flex flex-col p-6">
+            <div className="flex-1 flex flex-col min-h-screen">
                 <div className="flex-1">
                     {children}
                 </div>
