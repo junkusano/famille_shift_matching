0649b8c4d3c7acab7d7ba087cd533dbabcc3c711
junkusano <junkusano@shi-on.net>
2025-07-04 01:20:24 +0900

    install LW callback function take7

--- src/app/api/webhook/route.ts
+++ src/app/api/webhook/route.ts
@@ -1,13 +1,10 @@
 import { createClient } from '@supabase/supabase-js'
 import { NextRequest, NextResponse } from 'next/server'
-import { getAccessToken } from '@/lib/getAccessToken';
 
 const supabase = createClient(
     process.env.NEXT_PUBLIC_SUPABASE_URL!,
     process.env.SUPABASE_SERVICE_ROLE_KEY!
 )
-
-const accessToken = await getAccessToken();
 
 type Group = {
     id: string
