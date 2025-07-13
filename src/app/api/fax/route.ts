26ef799f209115c17dbd8db283c49fdd39ad634e
junkusano <junkusano@shi-on.net>
2025-07-13 13:02:52 +0900

    insatall portal/fax take10

--- src/app/api/fax/route.ts
+++ src/app/api/fax/route.ts
@@ -4,7 +4,7 @@ import { NextResponse } from "next/server";
 // Supabaseクライアントの生成
 const supabase = createClient(
   process.env.NEXT_PUBLIC_SUPABASE_URL!,
-  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
+  process.env.SUPABASE_SERVICE_ROLE_KEY!
 );
 
 // 型定義
