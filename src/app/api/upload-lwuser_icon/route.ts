8a923adead374a30053a001aa8de08c39477c306
junkusano <junkusano@shi-on.net>
2025-07-26 20:00:02 +0900

    modify apply org_icon for entories take18

--- src/app/api/upload-lwuser_icon/route.ts
+++ src/app/api/upload-lwuser_icon/route.ts
@@ -37,7 +37,7 @@ export async function POST(req: Request) {
         'Content-Type': 'application/json'
       },
       body: JSON.stringify({ 
-        fileName: userId,
+        fileName: iconUrl,
         fileSize: fileSize
       })
     });
