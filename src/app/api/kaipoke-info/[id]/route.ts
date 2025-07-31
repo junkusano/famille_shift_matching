0bb891d37f58b071c9271a03a796658f74d6a879
junkusano <junkusano@shi-on.net>
2025-07-31 22:04:54 +0900

    modify portal/kaipoke_info take2

--- src/app/api/kaipoke-info/[id]/route.ts
+++ src/app/api/kaipoke-info/[id]/route.ts
@@ -1,7 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 import { NextRequest, NextResponse } from 'next/server'
 
-// Supabaseクライアント作成
 const supabase = createClient(
   process.env.NEXT_PUBLIC_SUPABASE_URL!,
   process.env.SUPABASE_SERVICE_ROLE_KEY!
