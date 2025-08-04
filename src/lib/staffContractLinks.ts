83d25ff162f71b86c199b86c22dd6728b729d792
junkusano <junkusano@shi-on.net>
2025-08-04 16:15:10 +0900

    update kaipoke_info take76

--- src/lib/staffContractLinks.ts
+++ src/lib/staffContractLinks.ts
@@ -1,5 +1,5 @@
 export const staffContractLinks = {
-  employment: "https://www.digisigner.com/online-new/showTemplate?linkId=173c7d4d-adde-4909-8d51-39d30e06ef48",
+  employment: "https://www.digisigner.com/online/showTemplate?linkId=2e90ba98-8973-4e74-8244-9c2d9026f5cf",
   privacy: "https://www.digisigner.com/online/showTemplate?linkId=3859482d-e0df-494f-ae9e-8146ef298bb6",
   privateCar: "https://www.digisigner.com/online/showTemplate?linkId=840b3b68-f033-4323-af5f-cd9198f5f647",
 };
