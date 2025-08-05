8580672a84ea0b2d7a817c3649bc87b6d49c7a0c
junkusano <junkusano@shi-on.net>
2025-08-05 21:12:03 +0900

    install shift take15

--- src/app/portal/shift/page.tsx
+++ src/app/portal/shift/page.tsx
@@ -40,12 +40,16 @@ export default function ShiftPage() {
             if (userRecord?.user_id) {
                 setUserId(userRecord.user_id); // user_id（例えば、'junkusano'）を設定
 
+                alert("user_id:"+ userId);
+
                 //const formattedDate = format(shiftDate, "yyyy-MM-dd");
                 setCurrentDate(format(shiftDate, "Y年M月d日"));
 
                 // 現在の日付を基にシフトを取得する
                 const startOfDay = new Date(shiftDate.setHours(0, 0, 0, 0));  // 今日の00:00
                 const endOfDay = new Date(shiftDate.setHours(23, 59, 59, 999)); // 今日の23:59
+
+
                 
                 const { data: shiftsData } = await supabase
                     .from("shift_csinfo_postalname_view")
