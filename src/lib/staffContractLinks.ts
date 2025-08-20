f583cd914e99af77b3e2f784d8f716baee772cb0
junkusano <junkusano@shi-on.net>
2025-08-20 12:07:25 +0900

    update staff-contract take1

--- src/lib/staffContractLinks.ts
+++ src/lib/staffContractLinks.ts
@@ -1,5 +1,5 @@
 export const staffContractLinks = {
-  employment: "https://www.digisigner.com/online/showTemplate?linkId=2e90ba98-8973-4e74-8244-9c2d9026f5cf",
+  employment: "https://www.digisigner.com/online/showTemplate?linkId=5e64e9a0-a7a8-4308-8ec6-37bccc9a9f61",
   privacy: "https://www.digisigner.com/online/showTemplate?linkId=3859482d-e0df-494f-ae9e-8146ef298bb6",
   privateCar: "https://www.digisigner.com/online/showTemplate?linkId=840b3b68-f033-4323-af5f-cd9198f5f647",
 };
