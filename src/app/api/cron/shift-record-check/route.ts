40cc5c5b815c1675714db171425166739ac40201
junkusano <junkusano@shi-on.net>
2025-10-08 22:31:51 +0900

    insert shift-record/unfinish-alert take26

--- src/app/api/cron/shift-record-check/route.ts
+++ src/app/api/cron/shift-record-check/route.ts
@@ -80,11 +80,11 @@ export async function GET() {
             const userId = user.user_id;
 
             // テスト用ユーザー制限
-            if (user.user_id !== 'aihasegawa') {
+            if (user.user_id !== 'tomominakagaki') {
                 continue; // テスト用ユーザー以外はスキップ
             }
 
-            console.log(`[DEBUG] : ${userId} ('aihasegawa')`);
+            console.log(`[DEBUG] : ${userId} ('tomominakagaki')`);
 
             // --- (B) 利用者（Client）ループ (内側) ---
             for (const client of clientList) {
