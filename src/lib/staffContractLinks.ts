c141cb9e4a5325db9b5bec284928ef1f445ef56c
junkusano <junkusano@shi-on.net>
2026-01-27 10:39:25 +0900

    update empoly contruct take1

--- src/lib/staffContractLinks.ts
+++ src/lib/staffContractLinks.ts
@@ -1,5 +1,5 @@
 export const staffContractLinks = {
-  employment: "https://www.digisigner.com/online/showTemplate?linkId=4cfca6b2-4c34-4150-b632-7f1ddb4b47aa",
+  employment: "https://www.digisigner.com/online/showTemplate?linkId=e8fa223a-8090-4869-bfe5-adc073a62a09",
   privacy: "https://www.digisigner.com/online/showTemplate?linkId=3859482d-e0df-494f-ae9e-8146ef298bb6",
   privateCar: "https://www.digisigner.com/online/showTemplate?linkId=840b3b68-f033-4323-af5f-cd9198f5f647",
 };
