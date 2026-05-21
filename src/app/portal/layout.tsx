3f74507cbd7342142fc0aaa41816855caa486db0
unknown <servicesuport@shi-on.net>
2026-05-21 18:16:30 +0900

    shift_service 5 21 15

--- src/app/portal/layout.tsx
+++ src/app/portal/layout.tsx
@@ -140,13 +140,21 @@ function NavLinks({ role }: { role: string | null }) {
           月例会議参加チェック
         </Link>
       </li>
+      <li>
+        <Link
+          href="/portal/my-score"
+          className="text-blue-300 hover:underline"
+        >
+          パフォーマンススコア（開発中）
+        </Link>
+      </li>
       <li><Link href="/portal/shift-view" className="text-blue-300 hover:underline">シフト・勤務一覧</Link></li>
       <li><Link href="/portal/shift" className="text-blue-300 hover:underline">シフト・訪問記録</Link></li>
       <li><Link href="/portal/shift-coordinate" className="text-blue-300 hover:underline">ｼﾌﾄｾﾙﾌｺｰﾃﾞｨﾈｰﾄ（シフ子）</Link></li>
       <li><Link href="/portal/parking_cs_places" className="text-blue-300 hover:underline">駐車許可証申請</Link></li>
       <li><Link href="/portal/wf-seisan-shinsei" className="text-blue-300 hover:underline">清算・申請</Link></li>
-      <li><Link href="/portal/user_advance_payment_applications"className="text-blue-300 hover:underline">日払い申請フォーム（工事中）</Link></li>
-      <li><Link href="/portal/user_advance_payment_history"className="text-blue-300 hover:underline">日払い申請履歴（工事中）</Link></li>
+      <li><Link href="/portal/user_advance_payment_applications" className="text-blue-300 hover:underline">日払い申請フォーム（工事中）</Link></li>
+      <li><Link href="/portal/user_advance_payment_history" className="text-blue-300 hover:underline">日払い申請履歴（工事中）</Link></li>
       <li><Link className="text-blue-300 hover:underline" href="/portal/badge">職員証</Link></li>
       <li>
         <Link href="/lineworks-login-guide" className="hover:underline" target="_blank" rel="noopener noreferrer">
